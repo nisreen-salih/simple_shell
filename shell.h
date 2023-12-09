ce20babff092a9ebb3a10cdce738305d69c2cee6
Minatalla22 <menabashir1964@gmail.com>
2023-12-09 01:42:14 -0800

    gfcs

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef _SHELL_H
+#define _SHELL_H
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/star.h>
+#include <sys/wait.h>
+#define BUFFER_SIZE 1024
+extern char **environ;
+
+
+
+
+
+#endif
