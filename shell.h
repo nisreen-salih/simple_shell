fd4a56078ddd3699d3f0b7ae19b2377b7d4956d6
nisreen-salih <fg.nisreen@gmail.com>
2023-12-13 04:52:25 -0800

    1

--- shell.h
+++ shell.h
@@ -1,12 +1,19 @@
-#ifndef SHELL1_H
-#define SHELL1_H
-
-#include <stdio.h>
-#include <unistd.h>
+#ifndef _SHELL_H
+#define _SHELL_H
 #include <string.h>
+#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/star.h>
+#include <sys/wait.h>
+#define BUFFER_SIZE 1024
+void propmt(void);
+char clean_input(char *input, char *output);
+char *read_input(void);
+char *get_location(char *command);
+int extract_args(char *input, char*args[16]);
 
-char token(char *str);
+
 
 #endif
