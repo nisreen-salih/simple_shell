405918761bbf0b1824be11fe29c1d10c8f689c88
Minatalla22 <menabashir1964@gmail.com>
2023-12-13 15:53:57 -0800

    gdcf

--- task_1.c
+++ task_1.c
@@ -16,14 +16,14 @@ void prompt(void)
 void clean_input(char *input, char *output)
 {int i, cd = 0;
 
-	if (input == '#')
+	if (strcmp(input, "#") == 0)
 	{
 		output[i] = '\0';
 		return;
 	}
 	while (*input != '\0')
 	{
-		if (input == '#')
+		if (strcmp(input, "#") == 0)
 		{
 			input--;
 			if (*input == ' ')
@@ -35,6 +35,8 @@ void clean_input(char *input, char *output)
 			cd = 1;
 			output[i++] = *input;
 		}
+		else if (*input != ' ')
+		{
 		cd = 1;
 		output[i++] = *input;
 	}
@@ -44,7 +46,8 @@ void clean_input(char *input, char *output)
 		{
 			if (*input != ' ')
 			{
-				output[i++] == ' ';
+			if (output[i++] == ' ')
+			{
 				input--;
 				break;
 			}
@@ -59,13 +62,13 @@ void clean_input(char *input, char *output)
  * Return: 0 Success
  *
 */
-char read_input(void)
+char *read_input(void)
 {
-	char *input = Null;
-	char *output = Null;
+	char *input = NULL;
+	char *output = NULL;
 	size_t input_size = 0;
 	ssize_t read;
-
+	
 	while (1)
 	{
 		read = getline(&input, &input_size, stdin);
@@ -84,12 +87,87 @@ char read_input(void)
 		break;
 	}
 	output = malloc(sizeof(char) * (strlen(input) + 1));
-	if (output == Null)
+	if (output == NULL)
 	{
 		free(input);
-		return (Null);
+		return (NULL);
 	}
 	clean_input(input, output);
 	free(input);
 	return (output);
+}
+output);
+}
+
+
+/**
+ *get_location - This is the entry point of the code
+ *@command: lkdfm
+ *Return:0 Success
+ */
+char *get_location(char *command)
+{
+	char *path, *path_copy = NULL, *path_token, *file_path;
+	int command_length, directory_length;
+	struct stat buffer;
+
+	if (check_for_path(command) == 0)
+	{
+		path = getenv("PATH");
+		if (path)
+		{
+			path_copy = strdup(path);
+			command_length = strlen(command);
+			path_token = strtok(path_copy, ":");
+			while (path_token != NULL)
+			{directory_length = strlen(path_token);
+				file_path = malloc(command_length + directory_length + 2);
+				strcpy(file_path, path_token);
+				strcat(file_path, "/");
+				strcat(file_path, command);
+				strcat(file_path, "\0");
+				if (stat(file_path, &buffer) == 0)
+				{free(path_copy);
+					return (file_path);
+				}
+				else
+				{free(file_path);
+					path_token = strtok(NULL, ":"); }
+			}
+		}
+	}
+	else
+	{
+		if (path_copy != NULL)
+			free(path_copy);
+		if (stat(command, &buffer) == 0)
+			return (command);
+		else
+			return (NULL); }
+	if (path_copy != NULL)
+		free(path_copy);
+	return (NULL);
+}
+/**
+ *extract_args - This is the entry point of the code
+ *@in: jdkfvd
+ *@args: kejgnek
+ *
+ *Return:0 Success
+ */
+int extract_args(char *in, char *args[16])
+{
+	const char delm[2] = " ";
+	int i = 0;
+	char *token;
+
+	token = strtok(in, delm);
+	while (token != NULL)
+	{
+		args[i] = malloc(sizeof(char) * (strlen(token) + 1));
+		strcpy(args[i++], token);
+		token = strtok(NULL, delm);
+	}
+	args[i] = NULL;
+	return (i);
 }
